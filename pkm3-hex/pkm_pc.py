6af0cf9558eb77c70b75ce1236ff4f815343b986
Davide Laezza <truzzialrogo@gmx.com>
2025-04-29 22:54:05 +0200

    feature: add pkm-pc dump command

--- pkm3-hex/pkm_pc.py
+++ pkm3-hex/pkm_pc.py
@@ -1,13 +1,41 @@
+import sys
 from pathlib import Path
+from typing import Annotated
 
 import typer
 
+from . import PAGE_FOOTER_ADDRESS, PAGE_FOOTER_SIZE, PAGE_SIZE, STDOUT_ARG
+
 app = typer.Typer()
+
+PC_START_ADDRESS = 0xE004
+PC_PKM_LENGTH = 80
+PC_BOX_LENGTH = PC_PKM_LENGTH * 30
 
 
 @app.command()
-def dump(save_file: Path, box: int, slot: int):
-    pass
+def dump(
+    save_file: Path,
+    box: int,
+    slot: int,
+    output_file: Annotated[Path, typer.Argument()] = STDOUT_ARG,
+):
+    with open(save_file, "rb") as save:
+        save_bytes = save.read()
+
+    pkm_offset = (box - 1) * PC_BOX_LENGTH + (slot - 1) * PC_PKM_LENGTH
+    page_footers_in_offset = pkm_offset // PAGE_SIZE + (
+        1 if pkm_offset % PAGE_SIZE > PAGE_FOOTER_ADDRESS else 0
+    )
+
+    start = PC_START_ADDRESS + pkm_offset + page_footers_in_offset * PAGE_FOOTER_SIZE
+    pkm_bytes = save_bytes[start : start + PC_PKM_LENGTH]
+
+    if output_file == STDOUT_ARG:
+        sys.stdout.buffer.write(pkm_bytes)
+    else:
+        with open(output_file, "wb") as output:
+            output.write(pkm_bytes)
 
 
 @app.command()
